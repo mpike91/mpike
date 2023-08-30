57f89f89ab99528618188333636011bb9827ee1a
Matt Pike <mattp91@gmail.com>
2023-08-30 14:24:05 -0400

    basic styling

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,20 +4,20 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <>
+    <div>
       <h1>Project Sandbox</h1>
       {loggedIn ? (
         <MainMenu setLoggedIn={setLoggedIn} />
       ) : (
         <Login setLoggedIn={setLoggedIn} />
       )}
-    </>
+    </div>
   );
 }
 
 function Login({ setLoggedIn }) {
   return (
-    <>
+    <div>
       <h3> Please login, or continue as guest.</h3>
       <div>
         <button>Login</button>
@@ -27,16 +27,18 @@ function Login({ setLoggedIn }) {
           Continue as Guest
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
 function MainMenu({ setLoggedIn }) {
   return (
-    <>
+    <div>
       <h3>Main Menu</h3>
-      <button onClick={() => setLoggedIn((log) => !log)}>Logout</button>
-    </>
+      <div>
+        <button onClick={() => setLoggedIn((log) => !log)}>Logout</button>
+      </div>
+    </div>
   );
 }
 
